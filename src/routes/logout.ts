6c14cb8344ee55e96cf513d7a1d9d4d52eded9e9
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2022-08-04 00:33:19 +0700

    (Deployment) another cookie problem

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.delete('/', (req, res) => {
     res.clearCookie('x-auth-token', {
+        httpOnly: true,
+        secure: true,
         sameSite: 'none'
     });
     res.send('Logout successful');
